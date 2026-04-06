bc86275a066f4fa92309c121ae9de105419e8c5c
Issam Kebdani <kebdaniissam780@gmail.com>
2026-04-06 23:47:49 +0100

    .

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:3000'],
+    origin: process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : ['http://bericolipro.linguaflo.me:5173', 'http://bericolipro.linguaflo.me:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:3000'],
     credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
